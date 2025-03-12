280e6fd58701cbfa3beebeaa77cd72481f477ccb
CodeByMaks <maks667741@gmail.com>
2025-03-12 17:58:21 +0500

    fix v2

--- my-app/src/shared/ui/table/table.tsx
+++ my-app/src/shared/ui/table/table.tsx
@@ -1,15 +1,22 @@
-export function Table({children}){
-	return <table>
-		{children}
-	</table>
-} 
-
-export function TableRow({children}){
-	return <tr>
-		{children}
-	</tr>
+import { ReactNode } from 'react'
+interface TableCellProps {
+	children: ReactNode
+}
+interface TableRowProps {
+	children: ReactNode
+}
+interface TableProps {
+	children: ReactNode
 }
 
-export function TableCell({children}) {
+export function Table({ children }: TableProps) {
+	return <table>{children}</table>
+}
+
+export function TableRow({ children }: TableRowProps) {
+	return <tr>{children}</tr>
+}
+
+export function TableCell({ children }: TableCellProps) {
 	return <td>{children}</td>
 }
