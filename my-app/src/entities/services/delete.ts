5df1eaeccecd0f9843015b5d300949ca692d757e
CodeByMaks <maks667741@gmail.com>
2025-03-12 17:49:29 +0500

    fix v2

--- my-app/src/entities/services/delete.ts
+++ my-app/src/entities/services/delete.ts
@@ -1,3 +1,5 @@
-export function deleteUser(data, id:number | string){
-	return data.filter((elem)=> elem.id != id)
+import { IUser } from '../../entities/table-user/model/type.ts';
+
+export function deleteUser(data: IUser[], id: number | string): IUser[] {
+  return data.filter((elem) => elem.id !== id);
 }
